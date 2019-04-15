bd4872e9df6b195bae4669dda79a78aaa698adc6
William Belle <william.belle@gmail.com>
2019-04-15 19:04:46 +0200

    improve test coverage

--- test/test-findMenu.js
+++ test/test-findMenu.js
@@ -3,15 +3,28 @@
  * See the LICENSE file for more details.
  */
 
-require('chai').should();
+const should = require('chai').should();
+const rewire = require('rewire');
 
 const epflMenuApi = require('../src/index.js');
 
-describe('epfl-menu-api findMenu', () => {
+describe('epfl-menu-api findMenu', function () {
+  this.timeout(10000);
+
   it('should contains at least 5 menus', function () {
     return epflMenuApi.findMenu().then((menus) => {
       var enoughMenus = Object.keys(menus).length > 4;
       enoughMenus.should.equal(true);
     });
+  });
+
+  it('should fail with a wrong service url', (done) => {
+    let epflMenuApiMock = rewire('../src/index.js');
+    epflMenuApiMock.__set__('MENUS_URL', 'foobar');
+    let result = epflMenuApiMock.findMenu();
+    result.then((response) => {
+      should.fail();
+      done();
+    }).catch((reason) => done());
   });
 });
