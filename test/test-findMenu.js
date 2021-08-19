583d3a5b6dba8950dedfc0c8a63a237c2255723c
William Belle <william.belle@gmail.com>
2021-08-19 20:07:21 +0200

    update tests with recents menus

--- test/test-findMenu.js
+++ test/test-findMenu.js
@@ -61,9 +61,9 @@ describe('epfl-menu-api findMenu', function () {
     );
   });
 
-  it('should contains at least 5 menus on 16/03/2021', function () {
+  it('should contains at least 5 menus on 16/08/2021', function () {
     return epflMenuApi.findMenu({
-      date: '16/03/2021',
+      date: '16/08/2021',
       tags: 'ViANde,Chicken,  Pizza '
     }).then((menus) => {
       const enoughMenus = Object.keys(menus).length > 4;
@@ -71,10 +71,10 @@ describe('epfl-menu-api findMenu', function () {
     });
   });
 
-  it('should contains at least 1 menus on 26/03/2021', function () {
+  it('should contains at least 1 menus on 16/08/2021', function () {
     return epflMenuApi.findMenu({
-      date: '26/03/2021',
-      restoId: 18,
+      date: '16/08/2021',
+      restoId: 19,
       tags: 'Végétarien'
     }).then((menus) => {
       const enoughMenus = Object.keys(menus).length > 0;
@@ -82,9 +82,9 @@ describe('epfl-menu-api findMenu', function () {
     });
   });
 
-  it('should parse menus of 26/03/2021', function () {
+  it('should parse menus of 16/08/2021', function () {
     return epflMenuApi.findMenu({
-      date: '26/03/2021',
+      date: '16/08/2021',
       language: 'fr'
     }).then((menus) => {
       const enoughMenus = Object.keys(menus).length > 4;
